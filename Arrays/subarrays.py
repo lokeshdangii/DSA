bf039ca96481e10768d95428459f64c6ab5e693f
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-01 03:57:37 +0530

    Subarrays and there sum in an array

--- Arrays/subarrays.py
+++ Arrays/subarrays.py
@@ -1,8 +1,18 @@
 arr = [2,4,6,8,10]
-
+ts = 0
+sum = 0
 for i in range(0,len(arr)):
+    
     for j in range(i,len(arr)):
+        sum = 0
         for k in range(i,j+1):
-            print(arr[k],end=" ")
+            sum += arr[k]
+            print(arr[k], end=" ")
+
+        print("\nsum of array :",sum)
+            
+        ts +=1
         print()
     print()
+
+print("Total Subarrays :", ts)
