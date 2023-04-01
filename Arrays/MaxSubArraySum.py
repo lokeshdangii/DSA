b95b438eaf7e1886201d45c1de68e9bd7ac563d3
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-01 17:53:50 +0530

    Max Subarray Sum of Array

---
+++ Arrays/MaxSubArraySum.py
@@ -0,0 +1,16 @@
+arr = [2,4,6,8,10]
+curr_sum = 0
+maxsum = -10000
+
+for i in range(0,len(arr)):
+    for j in range(i,len(arr)):
+        curr_sum = 0
+        for k in range(i,j+1):
+           curr_sum += arr[k]
+        # print("Current Sum of Subarray = ",curr_sum)
+
+        if maxsum < curr_sum:
+            maxsum = curr_sum
+
+print("Maximum Sum = ",maxsum)
+
