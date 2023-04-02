fa0713608ce12d9513c62e9eff49ca97853ed2e6
lokeshdangii <lokeshdangi1045@gmail.com>
2023-04-02 15:25:58 +0530

    Buy and Sell Stocks

--- Arrays/BuyAndSellStocks.py
+++ Arrays/BuyAndSellStocks.py
@@ -7,9 +7,11 @@ def buyAndSell(prices):
         if buyPrice<prices[i]:
             profit = prices[i]-buyPrice
             maxProfit = max(profit,maxProfit)
+            print("The BuyPrice is {} and maximum Profit is {}".format(buyPrice,maxProfit))
+            
         else:
             buyPrice = prices[i]
-        
+            print("The BuyPrice ==",buyPrice)
 
     return maxProfit
 
